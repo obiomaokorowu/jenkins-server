8319195eb0a5f54c675da713b502a93fdbd84e87
obiomaokorowu <109354887+obiomaokorowu@users.noreply.github.com>
2024-10-19 10:40:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ data "aws_ami" "ubuntu" {
 
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
     }
 
     filter {
@@ -126,16 +126,16 @@ resource "aws_instance" "ec2_instance1" {
     Name = "Jenkins-server"
   }
 }
-
-resource "aws_instance" "ec2_instance2" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_default_subnet.default_az1.id
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "may_key"
-  user_data            = "${file("docker-install.sh")}"
-
-  tags = {
-    Name = "Docker-server"
-  }
-}
+#
+#resource "aws_instance" "ec2_instance2" {
+# ami                    = data.aws_ami.ubuntu.id
+# instance_type          = "t2.micro"
+# subnet_id              = aws_default_subnet.default_az1.id
+# vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+# key_name               = "may_key"
+# user_data            = "${file("docker-install.sh")}"
+#
+# tags = {
+#   Name = "Docker-server"
+# }
+#}
